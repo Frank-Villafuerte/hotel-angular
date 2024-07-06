0f0cf9b04929e675ab69e0bcf2445cafbbf61840
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2024-07-06 06:40:45 -0500

    implementado base de componente home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent} from './header/header.component';
+import { HomeComponent} from './home/home.component';
 export const routes: Routes = [
-    { path: 'header', component: HeaderComponent }
+    { path: '', component: HomeComponent }
+    //{ path: 'header', component: HeaderComponent }
 ];
