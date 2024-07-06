3b139debb50eb26edbd9861acd82eb8bb1035e97
Frank Villafuerte <fvillafuerte@unsa.edu.pe>
2024-07-06 07:31:32 -0500

    Añadidos los demás componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,16 @@
 import { Routes } from '@angular/router';
 import { HeaderComponent} from './header/header.component';
 import { HomeComponent} from './home/home.component';
+import { RestaurantComponent} from './restaurant/restaurant.component';
+import { RoomComponent} from './room/room.component';
+import { AboutusComponent} from './aboutus/aboutus.component';
+import { BlogComponent} from './blog/blog.component';
+import { ContactComponent} from './contact/contact.component';
 export const routes: Routes = [
     { path: '', component: HomeComponent }
-    //{ path: 'header', component: HeaderComponent }
+    ,{ path: 'Rooms', component: RoomComponent }
+    ,{ path: 'Restaurants', component: RestaurantComponent }
+    ,{ path: 'AboutUs', component: AboutusComponent }
+    ,{ path: 'Blog', component: BlogComponent }
+    ,{ path: 'Contact', component: ContactComponent }
 ];
